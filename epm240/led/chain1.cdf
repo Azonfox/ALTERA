7a4985405d780d738a4c9c6ee576636fd8a53daa
Azonfox <azonfox@gmail.com>
2024-04-10 19:50:53 +0600

    EMP240 init

---
+++ epm240/led/chain1.cdf
@@ -0,0 +1,13 @@
+/* Quartus II Version 9.1 Build 350 03/24/2010 Service Pack 2 SJ Web Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(EPM240T100) Path("C:/altera/EPM240/LED/") File("liushuideng.pof") MfrSpec(OpMask(7) SEC_Device(EPM240T100) Child_OpMask(2 7 7));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
---
+++ epm240/verilog/v1.v
@@ -0,0 +1,15 @@
+module V1 ( input wire clk, output reg led);
+parameter PERIODLED=9999999;
+integer count=0;
+	always @ (posedge clk)	begin
+		if (count<=PERIODLED) begin                          
+			led<=0;
+			count=count+1;
+		  end                          
+		else if (count>=PERIODLED && count<=PERIODLED*2)
+		  begin
+			led<=1;
+			count=count+1;
+		  end else count=0;
+		end              
+endmodule
