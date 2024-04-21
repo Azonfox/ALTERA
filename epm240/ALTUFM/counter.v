e8f9fbf2aaf7202e4f78adb37062a3b3b97c32ae
Azonfox <azonfox@gmail.com>
2024-04-21 18:35:54 +0600

    ALTUFM add led2

--- epm240/ALTUFM/counter.v
+++ epm240/ALTUFM/counter.v
@@ -1,17 +1,28 @@
-module counter ( input clk,  output  reg  led );
+module counter ( input clk,  output  reg  led1, output reg led2 );
 
     parameter PERIODLED=2;
-//altumf_osc osc(.oscena(1'b1),.osc(clk));
-    integer count=0;
-    always @ (posedge clk)	begin
-	if (count<=PERIODLED) begin                          
-	    led<=0;
+              //altumf_osc osc(.oscena(1'b1),.osc(clk));
+    integer countone=0; // Счетчик одноразовый
+    integer count=0;    // Счетчик периодичный
+    always @ (posedge clk)	
+      begin
+	// Вариант непосредственного указания
+	if (countone<5)      led1<=0;
+	if (countone>=5 && countone<10)     led1<=1;
+	else led1<=0;
+	countone=countone+1;
+
+	// Вариант периодичности
+	if (count<=PERIODLED) 
+	   begin                          
+	    led2<=0;
 	    count=count+1;
 	  end                          
 	else if (count>=PERIODLED && count<=PERIODLED*2)
-	  begin
-	    led<=1;
+	   begin
+	    led2<=1;
 	    count=count+1;
-	  end else count=0;
-	end              
+	   end 
+         else count=0;
+    end              
 endmodule
--- epm240/ALTUFM/tcounter.v
+++ epm240/ALTUFM/tcounter.v
@@ -2,19 +2,21 @@
 module test_counter();
 
 reg clk;
-wire led;
+wire led1, led2;
 
 //устанавливаем экземпляр тестируемого модуля
-counter counter_inst( clk, led );
+counter counter_inst( clk, led1, led2 );
 //моделируем сигнал тактовой частоты
 always
-  #10 clk = ~clk;
+  #10  clk = ~clk;
+
 //от начала времени...
 initial
 begin
   clk = 0;
   //пауза длительностью "50"
-  #50  $display("Running---");
+  #50  $display("Running---%d",$time);
+       //$monitor(led);        
 end 
 
 //заканчиваем симуляцию в момент времени "400"
@@ -32,6 +34,6 @@ end
 
 //наблюдаем на некоторыми сигналами системы
 initial
-$monitor($stime,,  clk,,, led );
+$monitor($stime,,,  clk,,, led1,,, led2 );
 
 endmodule
